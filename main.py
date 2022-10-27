4d5693a2b6861a6f286739aa63482171393d974b
Thales Gabriel <90735076+thalesgfelix@users.noreply.github.com>
2022-10-27 16:26:52 +0000

    new

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Primeiro projeto Python
 
 #Sistema de votos
-import os
+"""import os
 
 votos_Lula = 0
 votos_Bolsonaro = 0
@@ -24,4 +24,38 @@ while True:
   except:
     print("Digite apenas 1 ou 2")
   
-  os.system('clear')
+  os.system('clear')"""
+
+#Aula 2
+
+'''nome = input("Informe seu nome: ")
+print(f"Boa noite, seu nome é {nome}")
+
+idade = int(input("Qual a sua idade? "))
+print(f"A sua idade é  {idade} ano(s)")
+
+dobIdade = idade * 2
+print("O dobro da sua idade é: ", dobIdade)
+
+#Estrutura condicional - o famoso "SE" (if)
+#Se a pessoa for maior de idade, mostre "Você é maior de idade, ótimo! Já pode beber ou dirigir"
+if idade >= 18:
+  print("Você é maior de idade, ótimo! Já pode beber ou dirigir")
+
+else :
+  print("Você é jovem ainda, jovem ainda...")
+
+genero = input("Informe o gênero M = Masculino, F = Feminino: ")
+if idade >= 18 and genero == "M":
+  print ("... e você precisa/precisou prestar o serviço obrigatório'''
+
+# Prova
+
+nome = input("Informe seu nome: ")
+nota = float(input("Digite sua nota de 0 a 10: "))
+if nota == 10:
+  print (f"{nome}, você é o bichão mermo...")
+elif (nota >= 6 and nota < 10):
+  print(f"{nome}, bom trabalho")
+else:
+  print("Burro, não tirou dez...")
